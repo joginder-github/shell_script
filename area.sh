87fd17bfe6817c5e47ed6f4472ece14fcd6b6df8
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-31 23:15:50 +0530

    area_and_perimeter_of-circle

---
+++ area.sh
@@ -0,0 +1,7 @@
+echo "Enter radius of circle"
+read radius
+perimeter=`expr 2 \* $radius \* 22\/7`
+area=`expr $radius \* $radius \* 22\/7`
+echo "area of circle is "$area
+echo "perimeter of circle is "$perimeter
+
