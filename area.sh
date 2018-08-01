4d34f18eca5cc0206edd7c2ce0983ef2a7ba5757
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-08-01 19:50:04 +0530

    deleted

--- area.sh
+++
@@ -1,7 +0,0 @@
-echo "Enter radius of circle"
-read radius
-perimeter=`expr 2 \* $radius \* 22 / 7`
-area=`expr $radius \* $radius \* 22 / 7`
-echo "area of circle is $area"
-echo "perimeter of circle is $perimeter"
-
