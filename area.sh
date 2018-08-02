7ebb7b55056b0070306f9e98bda1f7d583885265
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-08-02 21:30:00 +0530

    area_and_circumference_of_circle

--- area.sh
+++ area.sh
@@ -5,3 +5,10 @@ area=`expr $radius \* $radius \* 22 / 7`
 echo "area of circle is $area"
 echo "circumference of circle is $circumference"
 
+
+# output:
+
+# Enter radius of circle
+# 22
+# area of circle is 1521
+# circumference of circle is 138
